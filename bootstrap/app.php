893943a1246bcb31562b5d0aa90a0b332321f7e0
Muris Ceman <murisceman@gmail.com>
2020-04-26 17:05:42 +0200

    Fix alternative autoload.php path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,7 @@
 <?php
 
-if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
-    $loader = require __DIR__ . '/../../../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../../../autoload.php')) {
+    $loader = require __DIR__ . '/../../../autoload.php';
 } else {
     $loader = require __DIR__ . '/../vendor/autoload.php';
 }
